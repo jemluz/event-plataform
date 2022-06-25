66aa6a782e69bee64877a49e038257d7a387526b
Jemima Luz <contatojemima@gmail.com>
2022-06-24 22:00:34 -0300

    feat: Sidebar implementation
    feat: header implementation
    
    - create EventPage
    - import Header into EventPage
    - create Logo component
    - import Logo into Header
    - changed color pallet on tailwind config
    - header styling
    - reorganize the App.tsx with EventPage
    
    feat: prepare EventPage to implements Sidebar
    
    - enable font-smoothing
    - increment EventPage with Sidebar
    - create Video component
    -
    
    feat: lesson implementation
    
    - npm i date-fns
    - npm i phosphor-react
    - create Lesson interface to be used on props
    - Lesson tags markup
    - Lesson styling
    - import functions from "date-fns" to treat date
    
    feat: sidebar implementation
    
    - use gql and useQuery to mount graphcms query request
    - create GetLessonsQueryResponse interface to define lessons request type
    - Sidebar tags markup
    - Sidebar styling
    - use request data to render lesson list

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,8 @@
-import { gql, useQuery } from "@apollo/client"
-// import { useEffect } from "react"
-// import { client } from "./lib/apollo"
-
-// useEffect é uma forma de fazer a requisição pelo react (ele vai dentro do App())
-// useEffect(() => {
-//   client.query({
-//     query: GET_LESSONS_QUERY
-//   }).then(response => {
-//     console.log(response.data)
-//   })
-// }, [])
-
-// useQuery é um hook (que vai substituir o useEffect), uma função que adiciona um comportamento ao componente
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-} 
+import { EventPage } from "./pages/Event"
 
 function App() {
-  // faz a requisição
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-  console.log(data);
-
   return (
-    // itera a response e faz o biding em lista
-    <ul>
-      {
-        data?.lessons.map(lesson => {
-          return <li>{lesson.title}</li>
-        })
-      }
-    </ul>
+    <EventPage/>
   )
 }
 
