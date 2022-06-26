39c9fced9d2cfa74ea864a666918be3bbdbce823
Jemima Luz <contatojemima@gmail.com>
2022-06-26 11:53:58 -0300

    feat: Subscribe implementation
    - inclusion of home assets
    - add bg-blur at tailwind config
    - create SubscribePage
    - include SubscribePage on router
    - SubscribePage tags markup
    - SubscribePage styling
    - use gql e useMutation to mount createSubscribe request
    - include form input data into createSubscribe request
    - create function handleSubscribe to call createSubscribe request
    - loading implementation while request is runing
    - npm i classnames
    - use of the classnames library to improve conditional styles
    - condicional lesson state style
    - protection of sensitive data with environment variables
    - reorganize apollo client to work with env variables

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { EventPage } from "./pages/Event";
+import { Subscribe } from "./pages/Subscribe";
 
 export function Router() {
     return (
         <Routes>
-            <Route path="/" element={<p>adad</p>} />
+            <Route path="/" element={<Subscribe/>} />
             <Route path="/event" element={<EventPage/>} />
             <Route path="/event/lesson/:slug" element={<EventPage/>} />
         </Routes>
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4t1zz472txk01wbfasv0zn6/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
